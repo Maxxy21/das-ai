840fe3cda6e58c2ea61eaf5b1f3a80a2f80c814c
Maxwell Aboagye <nanabaah12@gmail.com>
2024-03-02 15:29:57 +0100

    card actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import {Toaster} from "@/components/ui/sonner";
 import {ConvexClientProvider} from "@/providers/convex-client-provider";
+import {ModalProvider} from "@/providers/modal-provider";
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -21,7 +22,8 @@ export default function RootLayout({
         <html lang="en">
         <body className={inter.className}>
         <ConvexClientProvider>
-            <Toaster />
+            <Toaster/>
+            <ModalProvider/>
             {children}
         </ConvexClientProvider>
         </body>
