3f225453e63caa54ccd699f23485301c7a58f620
Maxwell Aboagye <nanabaah12@gmail.com>
2024-03-02 14:35:00 +0100

    created the board list

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,13 +1,39 @@
+"use client";
+
 import Image from "next/image";
-import {Button} from "@/components/ui/button";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { useOrganization } from "@clerk/nextjs";
+
+import { api } from "@/convex/_generated/api";
+import { Button } from "@/components/ui/button";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 
 export const EmptyBoards = () => {
+    const router = useRouter();
+    const { organization } = useOrganization();
+    const { mutate, pending } = useApiMutation(api.board.create);
+
+    const onClick = () => {
+        if (!organization) return;
+
+        mutate({
+            orgId: organization.id,
+            title: "Untitled"
+        })
+            .then((id) => {
+                toast.success("Board created");
+                router.push(`/board/${id}`);
+            })
+            .catch(() => toast.error("Failed to create board"));
+    };
+
     return (
         <div className="h-full flex flex-col items-center justify-center">
             <Image
                 src="/note.svg"
-                height={140}
-                width={140}
+                height={110}
+                width={110}
                 alt="Empty"
             />
             <h2 className="text-2xl font-semibold mt-6">
@@ -16,9 +42,8 @@ export const EmptyBoards = () => {
             <p className="text-muted-foreground textg-sm mt-2">
                 Start by creating a board for your organization
             </p>
-
             <div className="mt-6">
-                <Button size="lg">
+                <Button disabled={pending} onClick={onClick} size="lg">
                     Create board
                 </Button>
             </div>
