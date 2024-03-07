cb9ee8926b225572b673d66fea0e29652b4dc3fd
Maxxy21 <nanabaah12@gmail.com>
2024-03-07 01:11:09 +0100

    fixed delete routes

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -1,138 +1,188 @@
 "use client";
 
-import { memo } from "react";
-import { BringToFront, SendToBack, Trash2 } from "lucide-react";
+import {memo} from "react";
+import {BringToFront, SendToBack, Trash2} from "lucide-react";
 
-import { Hint } from "@/components/hint";
-import { Camera, Color } from "@/types/canvas";
-import { Button } from "@/components/ui/button";
-import { useMutation, useSelf } from "@/liveblocks.config";
-import { useDeleteLayers } from "@/hooks/use-delete-layers";
-import { useSelectionBounds } from "@/hooks/use-selection-bounds";
+import {Hint} from "@/components/hint";
+import {Camera, Color} from "@/types/canvas";
+import {Button} from "@/components/ui/button";
+import {useMutation, useSelf} from "@/liveblocks.config";
+import {useDeleteLayers} from "@/hooks/use-delete-layers";
+import {useSelectionBounds} from "@/hooks/use-selection-bounds";
 
-import { ColorPicker } from "./color-picker";
+import {ColorPicker} from "./color-picker";
 
 interface SelectionToolsProps {
-  camera: Camera;
-  setLastUsedColor: (color: Color) => void;
+    camera: Camera;
+    setLastUsedColor: (color: Color) => void;
 };
 
 export const SelectionTools = memo(({
-  camera,
-  setLastUsedColor,
-}: SelectionToolsProps) => {
-  const selection = useSelf((me) => me.presence.selection);
+                                        camera,
+                                        setLastUsedColor,
+                                    }: SelectionToolsProps) => {
+    const selection = useSelf((me) => me.presence.selection);
 
-  const moveToFront = useMutation((
-    { storage }
-  ) => {
-    const liveLayerIds = storage.get("layerIds");
-    const indices: number[] = [];
+    const moveToFront = useMutation((
+        {storage}
+    ) => {
+        const liveLayerIds = storage.get("layerIds");
+        const indices: number[] = [];
 
-    const arr = liveLayerIds.toImmutable();
+        const arr = liveLayerIds.toImmutable();
 
-    for (let i = 0; i < arr.length; i++) {
-      if (selection.includes(arr[i])) {
-        indices.push(i);
-      }
+        for (let i = 0; i < arr.length; i++) {
+            if (selection.includes(arr[i])) {
+                indices.push(i);
+            }
+        }
+
+        for (let i = indices.length - 1; i >= 0; i--) {
+            liveLayerIds.move(
+                indices[i],
+                arr.length - 1 - (indices.length - 1 - i)
+            );
+        }
+    }, [selection]);
+
+    const moveToBack = useMutation((
+        {storage}
+    ) => {
+        const liveLayerIds = storage.get("layerIds");
+        const indices: number[] = [];
+
+        const arr = liveLayerIds.toImmutable();
+
+        for (let i = 0; i < arr.length; i++) {
+            if (selection.includes(arr[i])) {
+                indices.push(i);
+            }
+        }
+
+        for (let i = 0; i < indices.length; i++) {
+            liveLayerIds.move(indices[i], i);
+        }
+    }, [selection]);
+
+    const setFill = useMutation((
+        {storage},
+        fill: Color,
+    ) => {
+        const liveLayers = storage.get("layers");
+        setLastUsedColor(fill);
+
+        selection.forEach((id) => {
+            liveLayers.get(id)?.set("fill", fill);
+        })
+    }, [selection, setLastUsedColor]);
+
+    const deleteLayers = useDeleteLayers();
+
+
+    const handleDelete = useMutation(async (
+        {storage},
+    ) => {
+        const liveLayers = storage.get("layers");
+        await deleteLayers();
+        try {
+            for (const id of selection) {
+                liveLayers.delete(id);
+                console.log("id", id);
+                const response = await fetch('/api/delete-layer', {
+                    method: 'DELETE',
+                    body: JSON.stringify(
+                        {
+                            id: id,
+                        })
+                })
+                if (!response.ok) throw Error("Status code: " + response.status);
+            }
+        } catch (e) {
+            console.error(e)
+        }
+
+
+    }, [selection]);
+
+
+    // const handleDelete = async () => {
+    //   try {
+    //     const response = await fetch('/api/delete-layer', {
+    //       method: 'DELETE',
+    //       headers: {
+    //         'Content-Type': 'application/json', // Ensure you have the correct content type for JSON request body
+    //       },
+    //       body: JSON.stringify({
+    //         id: id,
+    //       }),
+    //     });
+    //     if (!response.ok) throw new Error("Status code: " + response.status);
+    //
+    //     // Handle success response
+    //     console.log("Layer deleted successfully");
+    //     // Here, you might want to update UI or state to reflect the deletion
+    //   } catch (error) {
+    //     console.error(error);
+    //     // Handle error (e.g., show an error message to the user)
+    //   }
+    // };
+
+    const selectionBounds = useSelectionBounds();
+
+    if (!selectionBounds) {
+        return null;
     }
 
-    for (let i = indices.length - 1; i >= 0; i--) {
-      liveLayerIds.move(
-        indices[i],
-        arr.length - 1 - (indices.length - 1 - i)
-      );
-    }
-  }, [selection]);
+    const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
+    const y = selectionBounds.y + camera.y;
 
-  const moveToBack = useMutation((
-    { storage }
-  ) => {
-    const liveLayerIds = storage.get("layerIds");
-    const indices: number[] = [];
-
-    const arr = liveLayerIds.toImmutable();
-
-    for (let i = 0; i < arr.length; i++) {
-      if (selection.includes(arr[i])) {
-        indices.push(i);
-      }
-    }
-
-    for (let i = 0; i < indices.length; i++) {
-      liveLayerIds.move(indices[i], i);
-    }
-  }, [selection]);
-
-  const setFill = useMutation((
-    { storage },
-    fill: Color,
-  ) => {
-    const liveLayers = storage.get("layers");
-    setLastUsedColor(fill);
-
-    selection.forEach((id) => {
-      liveLayers.get(id)?.set("fill", fill);
-    })
-  }, [selection, setLastUsedColor]);
-
-  const deleteLayers = useDeleteLayers();
-
-  const selectionBounds = useSelectionBounds();
-
-  if (!selectionBounds) {
-    return null;
-  }
-
-  const x = selectionBounds.width / 2 + selectionBounds.x + camera.x;
-  const y = selectionBounds.y + camera.y;
-
-  return (
-    <div
-      className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
-      style={{
-        transform: `translate(
+    return (
+        <div
+            className="absolute p-3 rounded-xl bg-white shadow-sm border flex select-none"
+            style={{
+                transform: `translate(
           calc(${x}px - 50%),
           calc(${y - 16}px - 100%)
         )`
-      }}
-    >
-      <ColorPicker
-        onChange={setFill}
-      />
-      <div className="flex flex-col gap-y-0.5">
-        <Hint label="Bring to front">
-          <Button
-            onClick={moveToFront}
-            variant="board"
-            size="icon"
-          >
-            <BringToFront />
-          </Button>
-        </Hint>
-        <Hint label="Send to back" side="bottom">
-          <Button
-            onClick={moveToBack}
-            variant="board"
-            size="icon"
-          >
-            <SendToBack />
-          </Button>
-        </Hint>
-      </div>
-      <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
-        <Hint label="Delete">
-          <Button
-            variant="board"
-            size="icon"
-            onClick={deleteLayers}
-          >
-            <Trash2 />
-          </Button>
-        </Hint>
-      </div>
-    </div>
-  );
+            }}
+        >
+            <ColorPicker
+                onChange={setFill}
+            />
+            <div className="flex flex-col gap-y-0.5">
+                <Hint label="Bring to front">
+                    <Button
+                        onClick={moveToFront}
+                        variant="board"
+                        size="icon"
+                    >
+                        <BringToFront/>
+                    </Button>
+                </Hint>
+                <Hint label="Send to back" side="bottom">
+                    <Button
+                        onClick={moveToBack}
+                        variant="board"
+                        size="icon"
+                    >
+                        <SendToBack/>
+                    </Button>
+                </Hint>
+            </div>
+            <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
+                <Hint label="Delete">
+                    <Button
+                        variant="board"
+                        size="icon"
+                        // onClick={deleteLayers}
+                        onClick={handleDelete}
+                    >
+                        <Trash2/>
+                    </Button>
+                </Hint>
+            </div>
+        </div>
+    );
 });
 
 SelectionTools.displayName = "SelectionTools";
