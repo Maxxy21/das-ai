d3aeec44da191fbae5e471aabd2e7c7fa42823d7
Maxxy21 <nanabaah12@gmail.com>
2024-03-17 20:51:20 +0100

    added vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Suspense } from "react";
 
 import { Toaster } from "@/components/ui/sonner";
@@ -30,6 +31,7 @@ export default function RootLayout({
             {children}
           </ConvexClientProvider>
         </Suspense>
+        <SpeedInsights />
       </body>
     </html>
   );
